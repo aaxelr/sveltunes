ccb0baae6239fee3d57d794701414e6a2cb5e938
aaxelr <antonio.axelsson@gmail.com>
2022-08-31 10:28:58 +0200

    update index route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { goto } from '$app/navigation';
+
+	let searchTerm = '';
+
+	function handleSearch() {
+		goto(searchTerm);
+	}
+</script>
+
+<h2>myTunez Search</h2>
+<form on:submit|preventDefault={handleSearch} >
+	<label>
+		Search artist, group or song
+		<input type="text" bind:value={searchTerm} />
+	</label>
+	<button type="submit">Search</button>
+</form>
+
+<style>
+	h2 {
+		font-size: 2.25rem;
+	}
+	label {
+		font-size: 1.25rem;
+	}
+</style>
