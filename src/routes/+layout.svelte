be9de97b0e71feb6125727e29514da9e1be978c9
aaxelr <antonio.axelsson@gmail.com>
2022-08-31 10:06:46 +0200

    add +layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<!--TODO: HEADER -->
+<main>
+	<slot />
+</main>
+
+<!--TODO: FOOTER -->
+<style>
+	:global(p, b, h1, h2, label, a) {
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
+			'Open Sans', 'Helvetica Neue', sans-serif;
+		color: #101010;
+	}
+
+	main {
+		max-width: 968px;
+		margin-inline: auto;
+	}
+</style>
