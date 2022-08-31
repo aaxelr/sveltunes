03750e7f116d9182531fec1ed489d48eb404bd6e
aaxelr <antonio.axelsson@gmail.com>
2022-08-31 10:30:04 +0200

    add SongCard and SongCardContainer

---
+++ src/routes/[searchTerm]/SongCard.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	export let song: any;
+	const fullArtistName = song.artistName;
+	const fullTrackName = song.trackName;
+	const trunkArtistName = fullArtistName.length > 25 ? fullArtistName.substring(0, 25).concat('...') : fullArtistName;
+	const trunkTrackName = fullTrackName.length > 25 ? fullTrackName.substring(0, 25).concat('...') : fullTrackName;
+</script>
+
+<div class="wrapper">
+	<img src={song.artworkUrl100} alt="" />
+	<div class="inner">
+		<b title={fullArtistName}>{trunkArtistName}</b>
+		<p title={fullTrackName}>{trunkTrackName}</p>
+		<!-- <a href="">View more</a> -->
+	</div>
+</div>
+
+<style>
+	.wrapper {
+		border: 1px solid salmon;
+		border-radius: 5px;
+		display: flex;
+		box-shadow: -5px 5px 5px rgba(250, 128, 114, 0.7);
+		max-height: 100px;
+	}
+	img {
+		width: 100px;
+		height: 100px;
+		padding-right: 0.5rem;
+	}
+	.inner {
+		display: inline-flex;
+		flex-direction: column;
+		align-items: baseline;
+		margin-top: 0.5rem;
+		max-width: 20rem;
+		overflow: hidden;
+		white-space: nowrap;
+	}
+	p {
+		margin: 0.5rem 0;
+		text-overflow: ellipsis;
+	}
+</style>
---
+++ src/routes/[searchTerm]/SongCardContainer.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import SongCard from './SongCard.svelte';
+
+	export let songResults: any[];
+</script>
+
+<div>
+	{#each songResults as song}
+		<SongCard {song} />
+	{/each}
+</div>
+
+<style>
+	div {
+		display: grid;
+		grid-template-columns: repeat(3, 1fr);
+    gap: 1rem;
+	}
+</style>
